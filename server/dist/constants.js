61ac36559b093fdce5f4130e20e83826791717f4
code-an-kit <ankitpathrikar@gmail.com>
2024-01-27 01:34:27 +0530

    commit

---
+++ server/dist/constants.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.COOKI_NAME = exports._prod_ = void 0;
+exports._prod_ = process.env.NODE_ENV !== 'production';
+exports.COOKI_NAME = 'qid';
+//# sourceMappingURL=constants.js.map
---
+++ server/dist/index.js
@@ -0,0 +1,77 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
+const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
+const hello_1 = require("./resolvers/hello");
+const post_1 = require("./resolvers/post");
+const User_1 = require("./resolvers/User");
+const express_session_1 = __importDefault(require("express-session"));
+const redis_1 = require("redis");
+const connect_redis_1 = __importDefault(require("connect-redis"));
+const cors_1 = __importDefault(require("cors"));
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
+    const emFork = orm.em.fork();
+    yield orm.getMigrator().up();
+    const app = express_1.default();
+    const redisClient = redis_1.createClient({ legacyMode: false });
+    redisClient.on("connect", () => console.log("Connected to Redis!"));
+    redisClient.on("error", (err) => console.log("Redis Client Error", err));
+    yield redisClient.connect();
+    app.use(cors_1.default({
+        origin: "http://localhost:3000",
+        credentials: true
+    }));
+    app.use(express_session_1.default({
+        name: constants_1.COOKI_NAME,
+        store: new connect_redis_1.default({
+            client: redisClient,
+            disableTouch: true,
+        }),
+        cookie: {
+            path: "/",
+            maxAge: 1000 * 60 * 60 * 24 * 365 * 1,
+            httpOnly: true,
+            sameSite: "lax",
+            secure: false,
+        },
+        saveUninitialized: false,
+        secret: "bhsbcjsbcsdcbskc",
+        resave: false,
+    }));
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: yield type_graphql_1.buildSchema({
+            resolvers: [hello_1.HelloResolver, post_1.PostResolver, User_1.UserResolver],
+            validate: false
+        }),
+        context: ({ req, res }) => ({ em: emFork, req, res })
+    });
+    yield apolloServer.start();
+    apolloServer.applyMiddleware({
+        app,
+        cors: false
+    });
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000');
+    });
+});
+main().catch((err) => {
+    console.log('error is', err);
+});
+//# sourceMappingURL=index.js.map
